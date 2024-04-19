67d7463fe38d533d12ca8ec3e1425ccf0a918ce8
Omkar <omkar6174@gmail.com>
2024-04-19 21:03:46 +0530

    update variation fix

--- src/components/variation/model.ts
+++ src/components/variation/model.ts
@@ -19,14 +19,23 @@ export class VariationModel extends DefaultModel {
     constructor() {
         super('variation');
     }
+    async update(id: number, data: Record<string, any>): Promise<Record<string, any>> {
+        data.additionalDetails = JSON.stringify(data.additionalDetails);
+        const [newRecord] = await knex(this.tableName).where({id}).update(data).returning('*');
+        newRecord.additionalDetails = JSON.parse(newRecord.additionalDetails);
+        return newRecord;
+    }
 
     async create(data: Record<string, any>): Promise<Record<string, any>> {
+        data.additionalDetails = JSON.stringify(data.additionalDetails);
         const [newRecord] = await knex(this.tableName).insert(data).returning('*');
         newRecord.additionalDetails = JSON.parse(newRecord.additionalDetails);
         return newRecord;
     }
     async delete(id: number): Promise<boolean> {
-        await knex(this.tableName).where({ id }).update({status: 2});
+        //await knex(this.tableName).where({ id }).update({status: 2});
+        await knex(this.tableName).where({ id }).delete();
+
         return true;
     }
 }
