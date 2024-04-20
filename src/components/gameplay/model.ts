39d05ff725cd215b87dd952c1e91b1fa484eb143
Omkar <omkar6174@gmail.com>
2024-04-20 23:07:03 +0530

    get all gameplay update

--- src/components/gameplay/model.ts
+++ src/components/gameplay/model.ts
@@ -18,6 +18,30 @@ export class GameplayModel extends DefaultModel {
         super('gameplay');
     }
 
+    async getAll(filters: Record<string, any> = {}): Promise<Record<string, any>[]> {
+        let query = knex(this.tableName)
+        .select(`${this.tableName}.*`, 'variation.*')
+        .leftJoin('variation', `${this.tableName}.variationId`, 'variation.id');
+
+        if (Object.keys(filters).length > 0) {
+            for (const key in filters) {
+                if (filters.hasOwnProperty(key)) {
+                    const value = filters[key];
+                    
+                    // If the filter value is a string, use 'like' for substring matching
+                    if (typeof value === 'string') {
+                        query = query.where(key, 'like', `%${value}%`);
+                    } else {
+                        // For other data types, use a standard equality check
+                        query = query.where(key, value);
+                    }
+                }
+            }
+        }
+
+        return query;
+    }
+
     async delete(id: number): Promise<boolean> {
         await knex(this.tableName).where({ id }).update({status: GameplayStatus.ARCHIVED});
         return true;
